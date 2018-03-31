91bae23c12b1c70729f6976e89c9d7033f7985b2
12dbilli18 <dombbilli@gmail.com>
2018-03-30 22:45:58 -0600

    Clean up

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -6,28 +6,31 @@ sys.setrecursionlimit(3000)
 #Initialize the list of previously found values. This is so that we know if we hit a loop and therefore solved the conjecture.
 values_list = []
 
-def calculate(index, value):
+def calculate(value):
+	index = 1
+	while True:
+		#See if we already had the value before. If so, the print the conjecture has been solved. If not the added it and keep calculating
+		if value not in values_list:
+			values_list.append(value)
+		else:
+			print ('Solved!')
+			return
 
-	#See if we already had the value before. If so, the print the conjecture has been solved. If not the added it and keep calculating
-	if value not in values_list:
-		values_list.append(value)
-	else:
-		print ('Conjecture Solved!')
-		return	
+		#If the value is even, divide it by 2. If the value is odd, multiple it by 3 and add 1.
+		if value % 2 == 0:
+			value = value / 2
+		else:
+			value = (3 * value) + 1
 
-	#If the value is even, divide it by 2. If the value is odd, multiple it by 3 and add 1.
-	if value % 2 == 0:
-		value = value / 2
-	else:
-		value = (3 * value) + 1
-
-	#Print the iteration this is and the value at this interation.
-	print ('Position '+ str(index) + ': '+ str(int(value)))
-
-	#If value is equal to 1, then the starting value didn't disprove the conjecture.
-	if value != 1:
-		calculate(index +1, value) 
-
-calculate(1, int(sys.argv[1]))
+			#Print the iteration this is and the value at this interation.
+		print ('Iteration '+ str(index) + ': '+ str(int(value)))
+			#If value is equal to 1, then the starting value didn't disprove the conjecture.
+		if value == 1:
+			print('\nIterations: ' + str(index))
+			break
+		index = index + 1
 
 
+def sanatizeArg(arg):
+	return 2
+calculate(int(sys.argv[1]))
